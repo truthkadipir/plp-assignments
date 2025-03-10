cca0070778985db7938f46d3073206a9f6dcb535
truth <trutherkadi@gmail.com>
2025-03-10 11:01:31 +0300

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def calculator():
+    num1 = input("enter the first number: ")
+    num2 = input("enter the second number: ")
+    operator = input("enter the operator (+, -, *, /): ")
+    if operator == "+":
+        print(int(num1) + int(num2))
+    elif operator == "-":
+        print(int(num1) - int(num2))
+    elif operator == "*":
+        print(int(num1) * int(num2))
+    elif operator == "/":
+        print(int(num1) / int(num2))
+    else:
+        print("invalid operator")
+    if __name__ == "__main__":
+        calculator()
