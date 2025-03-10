873e706d6cbe3b3b2fa0b2a21ffd4383b207e6dc
truther <trutherkadi@gmail.com>
2025-03-10 20:36:53 +0300

    list1

---
+++ list1.py
@@ -0,0 +1,14 @@
+def list1():
+    my_list = []
+    my_list.append(10)
+    my_list.append(20)
+    my_list.append(30)
+    my_list.append(40)
+    my_list.insert(1, 15)
+    my_list.extend([50, 60, 70])
+    del my_list[-1]
+    my_list.sort()
+    print(my_list.index(30))
+
+if __name__ == "__main__":
+    list1()
